762eb2b5232417b80d3adb5724c750f0144a954e
Rick <wrightricky64@gmail.com>
2021-10-02 01:46:49 +0100

    Add configjson deserialization template

---
+++ RedPandaBot.CLI/Configuration/ConfigJson.cs
@@ -0,0 +1,14 @@
+﻿namespace RedPandaBot.Configuration
+{
+    public struct ConfigJson
+    {
+        [JsonProperty("token")]
+        public string Token { get; private set; }
+
+        [JsonProperty("botname")]
+        public string BotName { get; private set; }
+
+        [JsonProperty("botavatarurl")]
+        public string BotAvatarUrl { get; private set; }
+    }
+}
